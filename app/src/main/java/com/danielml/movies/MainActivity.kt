08f85b5e0287f459f62977c19d0928467cfdc2ee
Daniel Martinez <danielmn685@gmail.com>
2022-04-17 00:35:22 -0500

    Added a menu bar and app icon

--- app/src/main/java/com/danielml/movies/MainActivity.kt
+++ app/src/main/java/com/danielml/movies/MainActivity.kt
@@ -2,10 +2,25 @@ package com.danielml.movies
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+
+    val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+    val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+    val navController = navHostFragment.navController
+    bottomNavigationView.setupWithNavController(navController)
+  }
+
+  fun signInTapped(view: View) {
+    Toast.makeText(this, "You Pressed sign in", Toast.LENGTH_SHORT).show()
   }
 }
